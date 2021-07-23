c1c3d6cdcd5a8b186539ceb93741b4a16aeb5cb5
Aditya Padhye <adityapadhye1507@gmail.com>
2021-07-23 18:09:53 -0400

    Trying graphql query

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,26 +1,109 @@
-import * as React from 'react';
-import { graphql } from 'gatsby';
-
-export const query = graphql`
-  query HomePageQuery {
-      allFile {
-      edges {
-        node {
-          id
-        }
+import React from "react"
+import { StaticQuery, graphql } from "gatsby"
+ 
+export default () => ( 
+  <StaticQuery
+  query={graphql`
+  {
+    site {
+      siteMetadata {
+        title
+        siteUrl
+        description
       }
     }
   }
-`
+  `
+  }
+  render={ data =>(
+      <div>
+        <h1>About {JSON.stringify(data)}</h1>
+        <h2>We're a very cool website you should return to often.</h2>
+      </div>
+    )
+  } 
+  />
+)
+ 
+//export default Page
+ 
 
-export function HomePage({data}) {
-
-  console.log(data);
+/* export const Page = () =>{
+  const { data } = useStaticQuery(query)
 
   return (
     <div>
-      Hello!
+      <h1>About {data.site.siteMetadata.title}</h1>
+      <h2>We're a very cool website you should return to often.</h2>
     </div>
-  );
-}
+  )
+} */
 
+/* export const Page = ({ data }) => <h3>{JSON.stringify(data, null, 4)}</h3>
+
+export const query = graphql`
+{
+  site {
+    siteMetadata {
+      title
+      siteUrl
+      description
+    }
+  }
+}
+` */
+
+
+/* export const Page = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        allFile {
+          edges {
+            node {
+              id
+            }
+          }
+        }
+      }
+    `}
+    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+  ></StaticQuery>
+) */
+
+/* export var Page = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allFile {
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `)
+  return <pre>{JSON.stringify(data, null, 4)}</pre>
+} */
+
+
+
+/* 
+const Page = () => (
+  <StaticQuery
+    query={graphql`
+      {
+        site {
+          siteMetadata {
+            title
+            siteUrl
+            description
+          }
+        }
+      }
+    `}
+    render={data => <pre>{JSON.stringify(data, null, 4)}</pre>}
+  ></StaticQuery>
+)
+
+export default Page */
